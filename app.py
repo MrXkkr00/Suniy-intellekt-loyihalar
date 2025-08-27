22a4b9e967ac6fe8e9d52e2e8b64b8eb1626da3a
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2025-08-27 16:46:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ from fastai.vision.all import *
 import pathlib
 import plotly.express as px
 import warnings
+import platform
+
+# Path fix (Windows bilan ishlash uchun)
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
+
+plt = platform.system()
+if plt=='Linux':pathlib.WindowsPath = pathlib.PosixPath
 
 # Ogohlantirishni o‘chirib qo‘yish
 warnings.filterwarnings("ignore", category=UserWarning, module="fastai.learner")
 
-# Path fix (Windows bilan ishlash uchun)
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+
 
 st.title("Transport klassifikatsiya qiluvchi model")
 
 # Modelni yuklash
-model = load_learner('./transport_model.pkl')
 
 # Fayl yuklash
 file = st.file_uploader('Rasm yuklash', type='jpg')
@@ -22,6 +27,7 @@ if file:
     st.image(file)
 
     img = PILImage.create(file)
+    model = load_learner('./transport_model.pkl')
 
     # Bashorat qilish
     pred, pred_id, probs = model.predict(img)
